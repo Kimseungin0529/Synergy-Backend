a196b5018d60cf687f4856783d630bf4de99c04f
leeminjung1 <vipminjung@gmail.com>
2025-03-11 15:20:58 +0900

    refactor: add manytomany

--- src/main/java/com/synergy/backend/domain/booth/model/Booth.java
+++ src/main/java/com/synergy/backend/domain/booth/model/Booth.java
@@ -1,37 +1,56 @@
 package com.synergy.backend.domain.booth.model;
 
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import com.synergy.backend.domain.conference.entity.Conference;
-import jakarta.persistence.*;
+import com.synergy.backend.domain.member.entity.Admin;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import static jakarta.persistence.FetchType.LAZY;
-import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class Booth {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "booth_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "booth_id")
+	private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+	@Column(nullable = false, length = 100)
+	private String name;
 
-    @Column(nullable = false, length = 100)
-    private String company;
+	@Column(nullable = false, length = 100)
+	private String company;
 
-    @Column(nullable = false)
-    private String location;
+	@Column(nullable = false)
+	private String location;
 
-    @Column(nullable = false, length = 1000)
-    private String description;
+	@Column(nullable = false, length = 1000)
+	private String description;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "conference_id")
-    private Conference conference;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "conference_id")
+	private Conference conference;
+
+	@ManyToMany(mappedBy = "booths")
+	private Set<Admin> admins = new HashSet<>();
+
+	public void addAdmin(Admin admin) {
+		this.admins.add(admin);
+		admin.getBooths().add(this);
+	}
 
 }
--- src/main/java/com/synergy/backend/domain/conference/entity/Conference.java
+++ src/main/java/com/synergy/backend/domain/conference/entity/Conference.java
@@ -1,72 +1,93 @@
 package com.synergy.backend.domain.conference.entity;
 
+import static jakarta.persistence.CascadeType.*;
+import static lombok.AccessLevel.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import com.synergy.backend.domain.booth.model.Booth;
 import com.synergy.backend.domain.conference.exception.InvalidLocationException;
 import com.synergy.backend.domain.conference.exception.InvalidNameException;
+import com.synergy.backend.domain.member.entity.Admin;
 import com.synergy.backend.domain.session.entity.Session;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static jakarta.persistence.CascadeType.REMOVE;
-import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class Conference {
 
-    public static final int MAX_NAME_LENGTH = 50;
-    public static final int MAX_LOCATION_LENGTH = 100;
+	public static final int MAX_NAME_LENGTH = 50;
+	public static final int MAX_LOCATION_LENGTH = 100;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "conference_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "conference_id")
+	private Long id;
 
-    @Column(nullable = false, length = MAX_NAME_LENGTH)
-    private String name;
+	@Column(nullable = false, length = MAX_NAME_LENGTH)
+	private String name;
 
-    @Embedded
-    private TimePeriod period;
+	@Embedded
+	private TimePeriod period;
 
-    @Column(nullable = false, length = MAX_LOCATION_LENGTH)
-    private String location;
+	@Column(nullable = false, length = MAX_LOCATION_LENGTH)
+	private String location;
 
-    @OneToMany(mappedBy = "conference", cascade = {CascadeType.PERSIST, REMOVE}, orphanRemoval = true)
-    private List<Session> sessions = new ArrayList<>();
+	@OneToMany(mappedBy = "conference", cascade = {CascadeType.PERSIST, REMOVE}, orphanRemoval = true)
+	private List<Session> sessions = new ArrayList<>();
 
-    @OneToMany(mappedBy = "conference", cascade = {CascadeType.PERSIST, REMOVE}, orphanRemoval = true)
-    private List<Booth> booths = new ArrayList<>();
+	@OneToMany(mappedBy = "conference", cascade = {CascadeType.PERSIST, REMOVE}, orphanRemoval = true)
+	private List<Booth> booths = new ArrayList<>();
 
-    private Conference(String name, TimePeriod period, String location) {
-        this.name = name;
-        this.period = period;
-        this.location = location;
-    }
+	@ManyToMany(mappedBy = "conferences")
+	private Set<Admin> admins = new HashSet<>();
 
-    public static Conference of(String name, TimePeriod period, String location) {
-        if (name.length() > MAX_NAME_LENGTH || name.isBlank()) {
-            throw new InvalidNameException();
-        }
+	private Conference(String name, TimePeriod period, String location) {
+		this.name = name;
+		this.period = period;
+		this.location = location;
+	}
 
-        if (location.length() > MAX_LOCATION_LENGTH || location.isBlank()) {
-            throw new InvalidLocationException();
-        }
-        return new Conference(name, period, location);
-    }
+	public static Conference of(String name, TimePeriod period, String location) {
+		if (name.length() > MAX_NAME_LENGTH || name.isBlank()) {
+			throw new InvalidNameException();
+		}
 
-    public void updateName(String name){
-        this.name = name;
-    }
-    public void updateLocation(String location){
-        this.location = location;
-    }
-    public void updatePeriod(TimePeriod period){
-        this.period = period;
-    }
+		if (location.length() > MAX_LOCATION_LENGTH || location.isBlank()) {
+			throw new InvalidLocationException();
+		}
+		return new Conference(name, period, location);
+	}
+
+	public void addAdmin(Admin admin) {
+		this.admins.add(admin);
+		admin.getConferences().add(this);
+	}
+
+	public void updateName(String name) {
+		this.name = name;
+	}
+
+	public void updateLocation(String location) {
+		this.location = location;
+	}
+
+	public void updatePeriod(TimePeriod period) {
+		this.period = period;
+	}
 }
