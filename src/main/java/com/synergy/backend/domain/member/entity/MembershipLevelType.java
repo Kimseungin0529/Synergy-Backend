0e525b4848865952675021bf87a23b0419f0ff40
leeminjung1 <vipminjung@gmail.com>
2025-03-11 16:37:17 +0900

    feat: Update membership level system

--- src/main/java/com/synergy/backend/domain/member/entity/MembershipLevelType.java
+++ src/main/java/com/synergy/backend/domain/member/entity/MembershipLevelType.java
@@ -8,7 +8,6 @@ public enum MembershipLevelType {
 	DEFAULT;
 
 	public static MembershipLevelType getMembershipLevel(int point) {
-		if (point >= 1500) return PLATINUM;
 		if (point >= 800) return GOLD;
 		if (point >= 300) return SILVER;
 		if (point >= 100) return BRONZE;
