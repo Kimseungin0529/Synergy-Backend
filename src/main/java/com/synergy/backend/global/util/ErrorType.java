2fa968da9989ed20d5d71bbd9920f41fcb539211
Kimseungin0529 <whffkaos007@naver.com>
2025-03-12 13:33:12 +0900

    feat : 외부 API 호출 예외코드  추가

--- src/main/java/com/synergy/backend/global/util/ErrorType.java
+++ src/main/java/com/synergy/backend/global/util/ErrorType.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorType {
 
-    _FILE_UPLOAD_FAIL(400, "잘못된 파일 형식이 존재합니다.");
+    _FILE_UPLOAD_FAIL(400, "잘못된 파일 형식이 존재합니다."),
+    _EXTERNAL_API_FAIL_CALL(500, "외부 API 호출 실패했습니다.");
 
     private final int code;
     private final String message;
