2c41fe15585d1536cd9d481b1df3488020768f26
Kimseungin0529 <whffkaos007@naver.com>
2025-03-20 16:58:32 +0900

    chore : build.sh 생성 - 수동 빌드 파일

---
+++ build.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+./gradlew build
+
+# Define variables for Docker Hub account and image details
+DOCKERHUB_USERNAME=tmdfl36
+IMAGE_NAME=synergy
+TAG=latest  # or specify your desired tag/version
+
+# Build the Docker image
+docker buildx build --platform linux/amd64 -t $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG .
+# Log in to Docker Hub (if not logged in)
+# docker login -u $DOCKERHUB_USERNAME
+
+# Push the Docker image to Docker Hub
+docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
+
