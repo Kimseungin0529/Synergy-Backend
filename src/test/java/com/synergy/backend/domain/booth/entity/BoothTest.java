e851bc942c3a007fa7754c10d6a201225e9c3147
taemin <jungtaemin1214@naver.com>
2025-03-24 10:28:38 +0900

    feat : 부스 참여자 관심 분야별 인원 수 조회 기능 추가

--- src/test/java/com/synergy/backend/domain/booth/entity/BoothTest.java
+++ src/test/java/com/synergy/backend/domain/booth/entity/BoothTest.java
@@ -3,7 +3,6 @@ package com.synergy.backend.domain.booth.entity;
 import com.synergy.backend.domain.conference.entity.Conference;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -14,20 +13,23 @@ public class BoothTest {
     void createBooth() {
         // given
         Conference conference = mock(Conference.class);
-        String name = "부스A";
-        String company = "회사A";
-        String location = "위치A";
-        String description = "설명A";
+        String companyName = "부스A";
+        String companyType = "회사A";
+        String boothLocation = "위치A";
+        Integer boothNumber = 101;
+        String boothDescription = "설명A";
+        byte[] image = null;
 
         // when
-        Booth booth = new Booth(name, company, location, description, conference);
+        Booth booth = new Booth(companyName, companyType, boothLocation, boothNumber, boothDescription, conference, image);
 
         // then
         assertThat(booth).isNotNull();
-        assertThat(booth.getName()).isEqualTo(name);
-        assertThat(booth.getCompany()).isEqualTo(company);
-        assertThat(booth.getLocation()).isEqualTo(location);
-        assertThat(booth.getDescription()).isEqualTo(description);
+        assertThat(booth.getCompanyName()).isEqualTo(companyName);
+        assertThat(booth.getCompanyType()).isEqualTo(companyType);
+        assertThat(booth.getBoothLocation()).isEqualTo(boothLocation);
+        assertThat(booth.getBoothNumber()).isEqualTo(boothNumber);
+        assertThat(booth.getBoothDescription()).isEqualTo(boothDescription);
         assertThat(booth.getConference()).isEqualTo(conference);
     }
 
@@ -36,15 +38,16 @@ public class BoothTest {
     void updateBoothInfo() {
         // given
         Conference conference = mock(Conference.class);
-        Booth booth = new Booth("부스A", "회사A", "위치A", "설명A", conference);
+        Booth booth = new Booth("부스A", "회사A", "위치A", 101, "설명A", conference, null);
 
         // when
-        booth.updateInfo("부스B", "회사B", "위치B", "설명B");
+        booth.updateInfo("부스B", "회사B", "위치B", 202, "설명B", null);
 
         // then
-        assertThat(booth.getName()).isEqualTo("부스B");
-        assertThat(booth.getCompany()).isEqualTo("회사B");
-        assertThat(booth.getLocation()).isEqualTo("위치B");
-        assertThat(booth.getDescription()).isEqualTo("설명B");
+        assertThat(booth.getCompanyName()).isEqualTo("부스B");
+        assertThat(booth.getCompanyType()).isEqualTo("회사B");
+        assertThat(booth.getBoothLocation()).isEqualTo("위치B");
+        assertThat(booth.getBoothNumber()).isEqualTo(202);
+        assertThat(booth.getBoothDescription()).isEqualTo("설명B");
     }
 }
