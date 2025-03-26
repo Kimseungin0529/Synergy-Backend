595f531f5ade84cb2194ceafc804c844897323bc
Kimseungin0529 <whffkaos007@naver.com>
2025-03-26 11:11:36 +0900

    refactor : ConferenceUpdateRequest 수정

--- src/main/java/com/synergy/backend/domain/conference/dto/requset/ConferenceUpdateRequest.java
+++ src/main/java/com/synergy/backend/domain/conference/dto/requset/ConferenceUpdateRequest.java
@@ -2,23 +2,38 @@ package com.synergy.backend.domain.conference.dto.requset;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public record ConferenceUpdateRequest(
-    String name,
+        String name,
+        String organizer,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    @Future(message = "시작 날짜는 미래여야 합니다.")
-    LocalDateTime startDate,
+        @NotNull(message = "시작 날짜는 필수입니다.")
+        @Future(message = "시작 날짜는 미래여야 합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDate startDate,
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    @Future(message = "종료 날짜는 미래여야 합니다.")
-    LocalDateTime endDate,
+        @NotNull(message = "시작 시간은 필수입니다.")
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime startTime,
 
-    String location,
-    String organizer,
-    String type
+        @NotNull(message = "종료 날짜는 필수입니다.")
+        @Future(message = "종료 날짜는 미래여야 합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDate endDate,
+
+        @NotNull(message = "종료 시간은 필수입니다.")
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime endTime,
+
+        String location,
+        String position,
+        String type
+
+
 ) {
 }
 
