7e76a4415dbd2c400c2e1b2d42165d2efa45a2da
Kimseungin0529 <whffkaos007@naver.com>
2025-03-26 10:32:58 +0900

    refactor : ConferenceCreateRequest 필드 변경

--- src/main/java/com/synergy/backend/domain/conference/dto/requset/ConferenceCreateRequest.java
+++ src/main/java/com/synergy/backend/domain/conference/dto/requset/ConferenceCreateRequest.java
@@ -5,22 +5,43 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record ConferenceCreateRequest(
         @NotBlank(message = "컨퍼런스명은 필수입니다. 공백 이하는 불가능합니다.")
         String name,
-        @Future(message = "시작 날짜는 미래여야 합니다.")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime startDate,
-        @Future(message = "종료 날짜는 미래여야 합니다.")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime endDate,
-        @NotBlank(message = "컨퍼런스 위치 정보는 필수입니다. 공백 이하는 불가능합니다.")
-        String location,
+
         @NotBlank(message = "주최자명은 필수입니다. 공백 이하는 불가능합니다.")
         String organizer,
+
+        @NotNull(message = "시작 날짜는 필수입니다.")
+        @Future(message = "시작 날짜는 미래여야 합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDate startDate,
+
+        @NotNull(message = "시작 시간은 필수입니다.")
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime startTime,
+
+        @NotNull(message = "종료 날짜는 필수입니다.")
+        @Future(message = "종료 날짜는 미래여야 합니다.")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDate endDate,
+
+        @NotNull(message = "종료 시간은 필수입니다.")
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime endTime,
+
+        @NotBlank(message = "컨퍼런스 장소 정보는 필수입니다. 공백 이하는 불가능합니다.")
+        String location,
+
+        @NotBlank(message = "컨퍼런스 위치 정보는 필수입니다. 공백 이하는 불가능합니다.")
+        String position,
+
         @NotBlank(message = "컨퍼런스 유형은 필수입니다. 공백 이하는 불가능합니다.")
         String type
 ) {
