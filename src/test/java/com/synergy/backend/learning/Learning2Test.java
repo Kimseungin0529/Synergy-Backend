face0cf9e66f18cafba773a7f700e292a6c730c8
Kimseungin0529 <whffkaos007@naver.com>
2025-03-28 13:51:50 +0900

    feat : 불필요한 LearningTest 삭제

--- src/test/java/com/synergy/backend/learning/Learning2Test.java
+++ src/test/java/com/synergy/backend/learning/Learning2Test.java
@@ -1,16 +1,23 @@
 package com.synergy.backend.learning;
 
 
+import com.synergy.backend.global.config.MailConfig;
+import com.synergy.backend.global.util.file.util.FileUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public class Learning2Test {
+
+    @MockitoBean
+    JavaMailSender mailSender;
 
     @DisplayName("test task 수행 시간 검증 테스트")
     @Test
