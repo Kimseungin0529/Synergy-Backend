216e882f71fa42d3ecb6ee2ba6f44d082b1f7ea1
Kimseungin0529 <whffkaos007@naver.com>
2025-03-28 13:18:16 +0900

    test : test task 수행 시간 비교를 위한 임시 테스트 클래스 생성

---
+++ src/test/java/com/synergy/backend/learning/Learning3Test.java
@@ -0,0 +1,23 @@
+package com.synergy.backend.learning;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class Learning3Test {
+
+    @DisplayName("test task 수행 시간 검증 테스트")
+    @Test
+    void test1() {
+        // given
+        // when
+        // then
+    }
+
+}
